e487c868732efd929abfca9e60b55276197c007e
Fahamin <fahamin00@gmail.com>
2020-02-17 15:06:21 +0600

    object to object

--- app/src/main/java/app/us/dev/rettofitjsondatafatch/StringRootACT.java
+++ app/src/main/java/app/us/dev/rettofitjsondatafatch/StringRootACT.java
@@ -1,7 +1,15 @@
 package app.us.dev.rettofitjsondatafatch;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
@@ -9,8 +17,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.lang.reflect.Method;
 
 public class StringRootACT extends AppCompatActivity {
+
+    RequestQueue requestQueue;
+    String url;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,15 +38,67 @@ public class StringRootACT extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-
+        requestQueue = Volley.newRequestQueue(this);
+        textView = findViewById(R.id.textnewId);
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
+                startActivity(new Intent(StringRootACT.this, MainActivity.class));
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
             }
         });
+        // url = "https://api.myjson.com/bins/19jzms";
+
+
+        url = "https://api.myjson.com/bins/1b1sd0";
+        getData();
+
+    }
+
+    private void getData() {
+
+        JsonObjectRequest objectRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+
+                String result = "";
+                try {
+                    JSONArray array = response.getJSONArray("employees");
+
+                    for (int i = 0; i < array.length(); i++) {
+                        JSONObject object = array.getJSONObject(i);
+
+                        String name = object.getString("firstname");
+
+
+                        JSONObject jsonObject = object.getJSONObject("wow");
+                        int age = jsonObject.getInt("age");
+
+                        result += name + " " + String.valueOf(age) + "\n\n";
+                        result += name + "\n";
+
+                        textView.append(result);
+
+
+                    }
+
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+
+        requestQueue.add(objectRequest);
     }
 
 }
